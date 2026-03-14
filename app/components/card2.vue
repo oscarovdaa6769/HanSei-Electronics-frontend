5e32e3e45b1e685b58e0fbf4ce7b0cdb849aa9a0
Oscar <oudomveasnadet67@gmail.com>
2026-03-14 16:19:37 +0700

    finish the card2 in leave request

--- app/components/card2.vue
+++ app/components/card2.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import status from '/components/status.vue'
-import leaveCard from '/components/leaveCard.vue'
-import quote from '/components/quote.vue'
-
 const props = defineProps({
-  shortName: String,
-  fullName: String,
-  dep: String,
-  id: String,
+    shortName: String,
+    fullName: String,
+    dep: String,
+    id: String,
+    status: String,
+    color: String,
+    type: String,
+    icon: String,
+    color2: String,
+    date: String,
+    date2: String,
+    period: String,
+    graph: String,
+    status2: String,
 })
 </script>
 
@@ -23,19 +29,37 @@ const props = defineProps({
                     <span class="text-gray-400 text-sm">{{ dep }} | ID: {{ id }}</span>
                 </div>
             </div>
-            <status status="Pending" color="backdrop-blur-md bg-yellow-500/20 text-warning"/>
+            <div :class="['flex items-center justify-center px-2 py-1 rounded-lg', color]">
+                {{ status }}
+            </div>
         </div>
-        <leaveCard type="Sick Leave" icon="solar:stethoscope-bold" color="bg-red-500/20 backdrop-blur-md text-danger"/>
-        <quote graph="Family vacation to Jeju Island. Need time off for travel and rest."/>
+        <div class="p-3 bg-light rounded-lg flex flex-col gap-3">
+            <div :class="['flex items-center gap-2 px-2 py-1 rounded-lg self-start', color2]">
+                <Icon :name="props.icon"/>
+                <span class="text-sm">{{ type }}</span>
+            </div>
+            <div class="flex justify-between items-center w-full px-2">
+                <div class="flex flex-col justify-between items-center">
+                    <span class="text-gray-400">From</span>
+                    <h1 class="">{{ date }}</h1>
+                </div>
+                <Icon name="solar:alt-arrow-right-line-duotone" size="30"/>
+                <div class="flex flex-col justify-between items-center">
+                    <span class="text-gray-400">To</span>
+                    <h1 class="">{{ date2 }}</h1>
+                </div>
+            </div>
+            <div class="flex items-center justify-center bg-blue-500/20 backdrop-blur-md text-primary rounded-lg px-3 py-2">
+                <span class="text-md">{{ period }}</span>
+            </div>
+        </div>
+        <div class="p-3 bg-light rounded-lg h-25 overflow-auto">
+            <p class=""><Icon name="bxs:quote-alt-left" size="15" class="mr-1"/> {{ graph }}</p>
+        </div>
         <div class="w-full h-[1px] bg-line"></div>
-        <span class="flex items-center gap-3 text-gray-500"><Icon name="solar:calendar-mark-bold"/> Requested: Mar 10, 2026</span>
+        <span class="flex items-center gap-3 text-gray-500"><Icon name="solar:calendar-mark-bold"/>{{ status2 }}</span>
         <div class="flex items-center gap-3">
-            <button class="bg-green-500/20 backdrop-blur-md text-success py-2 rounded-lg w-full flex items-center justify-center cursor-pointer hover:bg-green-500/30">
-                <Icon name="solar:check-circle-bold" size="20"/>
-            </button>
-            <button class="bg-red-500/20 backdrop-blur-md text-danger py-2 rounded-lg w-full flex items-center justify-center cursor-pointer hover:bg-red-500/30">
-                <Icon name="solar:close-circle-bold cursor-pointer hover:bg-" size="20"/>
-            </button>
+            <slot name="actions"></slot>
         </div>
     </div>
 </template>
