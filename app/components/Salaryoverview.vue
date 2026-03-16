82faf62e86417db0cb1f1a80f63d8e8dd5d186d2
sreanlita81-cpu <sreanlita81@gmail.com>
2026-03-16 16:36:01 +0700

    hello

---
+++ app/components/Salaryoverview.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="min-h-screen bg-slate-50 p-2 text-slate-900 font-sans">
+  
+  <div class="flex justify-end mb-6">
+    
+    <div class="bg-white rounded-xl p-6 border border-slate-200 shadow-sm w-full max-w-258">
+      <div class="flex items-center gap-2 mb-6">
+        <h2 class="text-lg font-semibold text-slate-800">Salary Overview</h2>
+      </div>
+      
+      <table class="w-full text-left">
+        <thead class="text-xs text-slate-500 uppercase border-b border-slate-100">
+           <tr>
+            <th class="pb-3">Employee</th>
+            <th class="pb-3">Department</th>
+            <th class="pb-3 text-blue-600">Net Salary</th>
+            <th class="pb-3">OT Hours</th>
+          </tr>
+        </thead>
+        <tbody class="text-sm">
+          <tr class="border-b border-slate-50">
+            <td class="py-3 font-medium">Emily Davis</td>
+            <td class="py-3 text-slate-600">IT Support</td>
+            <td class="py-3 text-blue-600 font-bold">$4,456</td>
+            <td class="py-3 text-slate-600">6h</td>
+          </tr>
+          <tr class="border-b border-slate-50">
+            <td class="py-3 font-medium"> David Miller</td>
+            <td class="py-3 text-slate-600">Finance</td>
+            <td class="py-3 text-blue-600 font-bold">$3,136</td>
+            <td class="py-3 text-slate-600">5h</td>
+          </tr>
+          <tr class="border-b border-slate-50">
+            <td class="py-3 font-medium"> Sarah Williams</td>
+            <td class="py-3 text-slate-600">Sales</td>
+            <td class="py-3 text-blue-600 font-bold">$3,456</td>
+            <td class="py-3 text-slate-600">4h</td>
+          </tr>
+          <tr class="border-b border-slate-50">
+            <td class="py-3 font-medium">ECarol White</td>
+            <td class="py-3 text-slate-600">Human Resources</td>
+            <td class="py-3 text-blue-600 font-bold">$3,456</td>
+            <td class="py-3 text-slate-600">2h</td>
+          </tr>
+          </tbody>
+      </table>
+    </div>
+    
+  </div>
+</div>
+</template>
---
+++ app/pages/attendance.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import secondBtn from '/components/secondBtn.vue'
+import card from '/components/card.vue'
+</script>
+
+<template>
+  <div class="p-10 w-full flex flex-col gap-12">
+    <div class="flex justify-between items-center">
+      <div class="flex flex-col gap-3">
+        <h1 class="text-4xl font-black uppercase">Attendance</h1>
+        <span class="text-gray-400">Welcome back, Minho! Here's what's happening today.</span>
+      </div>
+      <secondBtn label="Export" icon="solar:export-bold"/>
+    </div>
+    <div class="">
+      <div class="grid grid-cols-4 gap-6">
+        <card label="Total Employee" icon="solar:user-bold" :amount="156" iicon="solar:arrow-up-linear" message="+12 this month" note="text-success" color="bg-blue-500/20 backdrop-blur-md text-primary"/>
+        <card label="Present Today" icon="solar:user-check-bold" :amount="142" iicon="solar:arrow-up-linear" message="+91% attendance" note="text-success" color="bg-green-500/20 backdrop-blur-md text-success"/>
+        <card label="On Leave" icon="solar:calendar-mark-bold" :amount="8" iicon="" message="4 pending requests" color="bg-yellow-500/20 backdrop-blur-md text-warning"/>
+        <card label="Late Today" icon="solar:clock-circle-bold" :amount="156" iicon="solar:arrow-down-linear" message="+12 this month" note="text-danger" color="bg-red-500/20 backdrop-blue-md text-danger"/>
+      </div>
+    </div>
+  </div>
+</template>
